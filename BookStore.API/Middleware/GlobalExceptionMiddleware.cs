34a6645f0e832d7da23d1f0cc74badafe94dc506
WaleedEmad173 <waleed.emad173@gmail.com>
2026-07-02 08:37:37 +0300

    Add global exception and request logging middleware
    Added GlobalExceptionMiddleware for centralized exception handling with custom status codes and structured JSON responses. Introduced RequestLoggingMiddleware to log HTTP request and response details, including method, path, status code, and processing time.

---
+++ BookStore.API/Middleware/GlobalExceptionMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using BookStore.Application.Exceptions;
+using System.Net;
+
+namespace BookStore.API.Middleware
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionMiddleware> _Logger;
+        public GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
+        {
+            _Logger = logger;
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+
+            }
+            catch (Exception ex)
+            {
+
+                var (statuscode, message) = ex switch
+                {
+                    NotFoundException => (HttpStatusCode.NotFound, ex.Message),
+                    BadRequestException => (HttpStatusCode.BadRequest, ex.Message),
+                    DuplicateException => (HttpStatusCode.Conflict, ex.Message),
+                    _ => (HttpStatusCode.InternalServerError, "An unexpected error occurred!")
+                };
+                if (statuscode == HttpStatusCode.InternalServerError)
+                    _Logger.LogError(ex, $"An unexpected error occurred: {ex.Message}");
+                else
+                    _Logger.LogWarning("A handled error occurred: {Type} - {Message}", ex.GetType().Name, ex.Message);
+
+
+                context.Response.StatusCode = (int)statuscode;
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    status = (int)statuscode,
+                    message = message
+                });
+
+            }
+        }
+    }
+}
---
+++ BookStore.API/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace BookStore.API.Middleware
+{
+    public class RequestLoggingMiddleware
+    {
+        readonly private RequestDelegate _next;
+        readonly private ILogger<RequestLoggingMiddleware> _logger;
+        public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            _logger.LogInformation("=> Request: {Method} {Path} {Query}", context.Request.Method, context.Request.Path, context.Request.Query);
+            await _next(context);
+            _logger.LogInformation("<= Response: {Method} {Path} responed {StatusCode} in {Time}", context.Request.Method, context.Request.Path, context.Response.StatusCode, stopWatch.ElapsedMilliseconds);
+
+        }
+    }
+}
