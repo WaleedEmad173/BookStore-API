ff0f53951ac4977558e9f7d031e87959766d27bc
WaleedEmad173 <waleed.emad173@gmail.com>
2026-07-02 08:38:59 +0300

    Add custom exception classes for application errors
    Added BadRequestException, DuplicateException, and NotFoundException under BookStore.Application.Exceptions. These classes provide custom error handling with specific constructors for detailed messages, improving error reporting throughout the application.

---
+++ BookStore.Application/Exceptions/DuplicateException.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.Application.Exceptions
+{
+    public class DuplicateException : Exception
+    {
+        public DuplicateException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ BookStore.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string resource, int id) : base($"{resource} with id {id} not found!")
+        {
+        }
+    }
+}
